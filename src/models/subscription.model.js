e366d4e8c8159e2a666e11ca8736f373839acf41
appna_notes <sarveshpandit6769@gmail.com>
2025-12-30 18:01:54 +0530

    refactor subscription model for improved readability and consistency

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -1,27 +1,29 @@
-import mongoose, {Schema} from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
-const subscriptionSchema = new Schema({
-  subscriber: {
-    type: Schema.Types.ObjectId, // one who is subcribing 
-    ref: "User"
+const subscriptionSchema = new Schema(
+  {
+    subscriber: {
+      type: Schema.Types.ObjectId, // one who is subscribing
+      ref: "User",
+    },
+    channel: {
+      type: Schema.Types.ObjectId, // one to whom 'subscriber' is subscribing
+      ref: "User",
+    },
+    startDate: {
+      type: Date,
+      default: Date.now,
+    },
+    endDate: {
+      type: Date,
+      default: null,
+    },
+    status: {
+      type: String,
+      enum: ["active", "inactive", "expired"],
+      default: "active",
+    },
   },
-  channel: {
-    type: Schema.Types.ObjectId, // one who is being subscribed
-    ref: "User"
-  },
-  startDate: {
-    type: Date,
-    default: Date.now
-  },
-  endDate: {
-    type: Date,
-    default: null
-  },
-  status: {
-    type: String,
-    enum: ["active", "inactive", "expired"],
-    default: "active"
-  }
-}, { timestamps: true });
-
+  { timestamps: true }
+);
 export const Subscription = mongoose.model("Subscription", subscriptionSchema);
